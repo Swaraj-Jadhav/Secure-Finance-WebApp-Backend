8d180f03f2813930501d9a2d834475a61dfef1a7
Swaraj-Jadhav <swarajj762@gmail.com>
2026-02-22 11:07:00 +0530

    Conflicts Resolved

--- src/app.js
+++ src/app.js
@@ -1,23 +1,14 @@
 const express = require("express");
-//const cors = require('cors');
-//const loginRoutes = require('./authentication/login');
 const accountRoutes = require("./account/account.routes");
 const subaccountRoutes = require("./subaccount/subaccount.routes");
 const transactionRoutes = require("./transactions/transaction.route");
 const app = express();
-//app.use(cors());
 app.use(express.json());
-
-// Routes
-//app.use('/api/authentication', loginRoutes);
 app.use("/api/account", accountRoutes);
 app.use("/api/subaccounts", subaccountRoutes);
 app.use("/api/transactions", transactionRoutes);
 
-//app.listen(3000, () => {
-//  console.log('Backend running: http://localhost:3000');
-//})
-// Health check
+
 app.get("/health", (req, res) => {
   res.json({ status: "OK" });
 });
