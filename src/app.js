e41e79f6629f9157303167f918629146a31b6dcb
Swaraj-Jadhav <swarajj762@gmail.com>
2026-02-23 13:39:35 +0530

    misc

--- src/app.js
+++ src/app.js
@@ -1,13 +1,23 @@
+
 const express = require("express");
+const cors = require("cors");
+
+
+const authRoutes = require("./authentication/routes");
 const accountRoutes = require("./account/account.routes");
 const subaccountRoutes = require("./subaccount/subaccount.routes");
 const transactionRoutes = require("./transactions/transaction.route");
+
 const app = express();
+
+app.use(cors());
 app.use(express.json());
+
+
+app.use("/api/auth", authRoutes);
 app.use("/api/account", accountRoutes);
 app.use("/api/subaccounts", subaccountRoutes);
 app.use("/api/transactions", transactionRoutes);
-
 
 app.get("/health", (req, res) => {
   res.json({ status: "OK" });
